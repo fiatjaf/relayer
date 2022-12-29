9ba43260856037ea25cd002f469ced74a71c879c
0x0ff <0x0ff@onsats.org>
2022-12-29 15:10:14 +0100

    fix: pass unix timestamp to DB.Exec instead of Time

--- basic/main.go
+++ basic/main.go
@@ -40,7 +40,7 @@ func (r *Relay) Init() error {
 
 		for {
 			time.Sleep(60 * time.Minute)
-			db.DB.Exec(`DELETE FROM event WHERE created_at < $1`, time.Now().AddDate(0, -3, 0)) // 3 months
+			db.DB.Exec(`DELETE FROM event WHERE created_at < $1`, time.Now().AddDate(0, -3, 0).Unix()) // 3 months
 		}
 	}()
 
