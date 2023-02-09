c7d584da5495dc15633a89e2607b6024dcc8d610
Gustavo Chain <me@qustavo.cc>
2023-02-09 01:30:36 +0100

    remove duplicated nip09 handling

--- add-event.go
+++ add-event.go
@@ -9,28 +9,7 @@ import (
 
 func AddEvent(relay Relay, evt nostr.Event) (accepted bool, message string) {
 	store := relay.Storage()
-	advancedDeleter, _ := store.(AdvancedDeleter)
 	advancedSaver, _ := store.(AdvancedSaver)
-
-	if evt.Kind == 5 {
-		// event deletion -- nip09
-		for _, tag := range evt.Tags {
-			if len(tag) >= 2 && tag[0] == "e" {
-				if advancedDeleter != nil {
-					advancedDeleter.BeforeDelete(tag[1], evt.PubKey)
-				}
-
-				if err := store.DeleteEvent(tag[1], evt.PubKey); err != nil {
-					return false, fmt.Sprintf("error: failed to delete: %s", err.Error())
-				}
-
-				if advancedDeleter != nil {
-					advancedDeleter.AfterDelete(tag[1], evt.PubKey)
-				}
-			}
-		}
-		return true, ""
-	}
 
 	if !relay.AcceptEvent(&evt) {
 		return false, "blocked: event blocked by relay"
