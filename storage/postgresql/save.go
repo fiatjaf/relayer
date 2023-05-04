a7dbf7c4910a5daa4ad54423f3386e8edc7eec7b
bndw <ben@bdw.to>
2023-05-03 17:18:47 -0700

    refactor(postgres): Unit test SaveEvent
    Refactors SaveEvent so it's unit testable and adds tests to
    assert the current behavior.

--- storage/postgresql/save.go
+++ storage/postgresql/save.go
@@ -9,30 +9,13 @@ import (
 )
 
 func (b *PostgresBackend) SaveEvent(ctx context.Context, evt *nostr.Event) error {
-	// react to different kinds of events
-	if evt.Kind == nostr.KindSetMetadata || evt.Kind == nostr.KindContactList || (10000 <= evt.Kind && evt.Kind < 20000) {
-		// delete past events from this user
-		b.DB.ExecContext(ctx, `DELETE FROM event WHERE pubkey = $1 AND kind = $2`, evt.PubKey, evt.Kind)
-	} else if evt.Kind == nostr.KindRecommendServer {
-		// delete past recommend_server events equal to this one
-		b.DB.ExecContext(ctx, `DELETE FROM event WHERE pubkey = $1 AND kind = $2 AND content = $3`,
-			evt.PubKey, evt.Kind, evt.Content)
-	} else if evt.Kind >= 30000 && evt.Kind < 40000 {
-		// NIP-33
-		d := evt.Tags.GetFirst([]string{"d"})
-		if d != nil {
-			b.DB.Exec(`DELETE FROM event WHERE pubkey = $1 AND kind = $2 AND tagvalues && ARRAY[$3]`,
-				evt.PubKey, evt.Kind, d.Value())
-		}
+	deleteQuery, deleteParams, shouldDelete := deleteBeforeSaveSql(evt)
+	if shouldDelete {
+		_, _ = b.DB.ExecContext(ctx, deleteQuery, deleteParams...)
 	}
 
-	// insert
-	tagsj, _ := json.Marshal(evt.Tags)
-	res, err := b.DB.ExecContext(ctx, `
-        INSERT INTO event (id, pubkey, created_at, kind, tags, content, sig)
-        VALUES ($1, $2, $3, $4, $5, $6, $7)
-		ON CONFLICT (id) DO NOTHING
-    `, evt.ID, evt.PubKey, evt.CreatedAt, evt.Kind, tagsj, evt.Content, evt.Sig)
+	sql, params, _ := saveEventSql(evt)
+	res, err := b.DB.ExecContext(ctx, sql, params...)
 	if err != nil {
 		return err
 	}
@@ -59,4 +42,48 @@ func (b *PostgresBackend) AfterSave(evt *nostr.Event) {
       SELECT created_at FROM event WHERE pubkey = $1
       ORDER BY created_at DESC OFFSET 100 LIMIT 1
     )`, evt.PubKey, evt.Kind)
+}
+
+func deleteBeforeSaveSql(evt *nostr.Event) (string, []any, bool) {
+	// react to different kinds of events
+	var (
+		query        = ""
+		params       []any
+		shouldDelete bool
+	)
+	if evt.Kind == nostr.KindSetMetadata || evt.Kind == nostr.KindContactList || (10000 <= evt.Kind && evt.Kind < 20000) {
+		// delete past events from this user
+		query = `DELETE FROM event WHERE pubkey = $1 AND kind = $2`
+		params = []any{evt.PubKey, evt.Kind}
+		shouldDelete = true
+	} else if evt.Kind == nostr.KindRecommendServer {
+		// delete past recommend_server events equal to this one
+		query = `DELETE FROM event WHERE pubkey = $1 AND kind = $2 AND content = $3`
+		params = []any{evt.PubKey, evt.Kind, evt.Content}
+		shouldDelete = true
+	} else if evt.Kind >= 30000 && evt.Kind < 40000 {
+		// NIP-33
+		d := evt.Tags.GetFirst([]string{"d"})
+		if d != nil {
+			query = `DELETE FROM event WHERE pubkey = $1 AND kind = $2 AND tagvalues && ARRAY[$3]`
+			params = []any{evt.PubKey, evt.Kind, d.Value()}
+			shouldDelete = true
+		}
+	}
+
+	return query, params, shouldDelete
+}
+
+func saveEventSql(evt *nostr.Event) (string, []any, error) {
+	const query = `INSERT INTO event (
+	id, pubkey, created_at, kind, tags, content, sig)
+	VALUES ($1, $2, $3, $4, $5, $6, $7)
+	ON CONFLICT (id) DO NOTHING`
+
+	var (
+		tagsj, _ = json.Marshal(evt.Tags)
+		params   = []any{evt.ID, evt.PubKey, evt.CreatedAt, evt.Kind, tagsj, evt.Content, evt.Sig}
+	)
+
+	return query, params, nil
 }
