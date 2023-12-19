e61e21bcc1493d442f571eb0a2e6c5da282c6385
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-12-19 11:35:01 +0900

    fix build

--- handlers.go
+++ handlers.go
@@ -66,7 +66,7 @@ func (s *Server) doEvent(ctx context.Context, ws *WebSocket, request []json.RawM
 	hash := sha256.Sum256(evt.Serialize())
 	if id := hex.EncodeToString(hash[:]); id != evt.ID {
 		reason := "invalid: event id is computed incorrectly"
-		ws.WriteJSON(nostr.OKEnvelope{EventID: evt.ID, OK: false, Reason: &reason})
+		ws.WriteJSON(nostr.OKEnvelope{EventID: evt.ID, OK: false, Reason: reason})
 		return ""
 	}
 
